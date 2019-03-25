07667da87af666f6d29dfb352dcacf0132f8d957
vijayganesanece <47973535+vijayganesanece@users.noreply.github.com>
2019-03-25 15:58:18 +0530

    Create rev of string.c

---
+++ rev of string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+  int a[100],b,i,j;
+  scanf("%c",a[i]);
+  for(i=0;a[i]!='';i++)
+  {
+      b++;
+  }
+  for(i=0;a[i]!='';i++)
+  {
+  for(j=b;j=0;j--);
+  {
+  printf("%c",a[i]);
+  }
+  }
+  return 0;
+}
